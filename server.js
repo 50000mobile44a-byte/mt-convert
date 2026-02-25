2296749cab3bd3506ef7b7a9db43c24a588ef848
50000mobile44a-byte <50000mobile44a@gmail.com>
2026-02-25 21:05:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -73,6 +73,16 @@ app.post("/convert", upload.single("video"), (req, res) => {
             "-loop 0"
         ])
         .format("gif")
+        // .on("end", () => {
+        //     res.download(outputPath, "output.gif", () => {
+        //         fs.unlinkSync(inputPath);
+        //         fs.unlinkSync(outputPath);
+        //     });
+        // })
+        // .on("error", (err) => {
+        //     console.error(err);
+        //     res.status(500).send("Error");
+        // })
         .on("end", () => {
 
             res.setHeader("Content-Type", "image/gif");
@@ -98,8 +108,91 @@ app.post("/convert", upload.single("video"), (req, res) => {
         })
         .save(outputPath);
 });
+
+const { exec } = require("child_process");
+const path = require("path");
+
+
+
+const uploadNone = multer();
+
+app.post("/youtube", uploadNone.none(), (req, res) => {
+
+    console.log("BODY:", req.body);
+
+    const { url, type, quality } = req.body;
+
+    if (!url) {
+        return res.status(400).send("No URL");
+    }
+
+    if (!fs.existsSync("downloads")) {
+        fs.mkdirSync("downloads");
+    }
+
+    const output = path.join(__dirname, "downloads", "%(title)s.%(ext)s");
+
+    let command;
+
+    if (type === "mp3") {
+
+        let audioQuality = "192";
+
+        if (quality && quality !== "lowest") {
+            audioQuality = quality;
+        }
+
+        if (quality === "lowest") {
+            audioQuality = "64";
+        }
+
+        command = `yt-dlp -x --audio-format mp3 --audio-quality ${audioQuality}K -o "${output}" "${url}"`;
+
+    } else {
+
+        const videoFormat = `
+    bestvideo[vcodec^=avc1][ext=mp4]+bestaudio[ext=m4a]/
+    bestvideo[ext=mp4]+bestaudio/
+    best[ext=mp4]/
+    best
+    `.replace(/\s+/g, "");
+
+        command = `yt-dlp -f "${videoFormat}" --merge-output-format mp4 -o "${output}" "${url}"`;
+    }
+
+    console.log("COMMAND:", command);
+
+    exec(command, (error) => {
+
+        if (error) {
+            console.error("YT-DLP ERROR:", error);
+            return res.status(500).send("Download failed");
+        }
+
+        const files = fs.readdirSync("downloads");
+
+        if (!files.length) {
+            return res.status(500).send("No file created");
+        }
+
+        const latest = files.sort((a, b) =>
+            fs.statSync(path.join("downloads", b)).mtime -
+            fs.statSync(path.join("downloads", a)).mtime
+        )[0];
+
+        const filePath = path.join(__dirname, "downloads", latest);
+
+        res.download(filePath, latest, () => {
+            fs.unlinkSync(filePath);
+        });
+    });
+});
 // app.listen(3000, () => {
 //     console.log("Server running at http://localhost:3000");
+// });
+
+// app.listen(5000, () => {
+//     console.log("Server running at http://localhost:5000");
 // });
 
 const PORT = process.env.PORT || 3000;
